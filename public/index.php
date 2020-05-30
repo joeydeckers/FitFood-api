4f76cd91ecbf2156e714fc6a369938029a8f327d
JoeyDeckers <deckersjoey@gmail.com>
2020-05-30 16:12:55 +0200

    Removed something

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
