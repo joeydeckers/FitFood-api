3528af8310d342220545af243ab2132dc6a4d715
JoeyDeckers <deckersjoey@gmail.com>
2020-05-30 16:27:39 +0200

    Added header

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-
+header("Access-Control-Allow-Methods: *");
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
