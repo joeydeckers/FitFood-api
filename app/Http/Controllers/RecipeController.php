668d776ec491d98c620944950599eb1d2ac64663
JoeyDeckers <deckersjoey@gmail.com>
2020-06-01 16:40:27 +0200

    Test

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -154,6 +154,7 @@ class RecipeController extends Controller
             return response(['message' => "Not found"], 404);
         }
         if($request['recipe_photo'] == ""){
+            return $request['recipe_photo'];
             $recipe->update($request->all());
         }else{
             Recipe::create([
